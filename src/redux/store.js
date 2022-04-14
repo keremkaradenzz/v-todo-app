d394efbb26452a717e3f323f7791f9aa1c055cb6
kerem <keremkaradenzz@gmail.com>
2022-04-14 15:38:20 +0300

    delete redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,10 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers/rootReducer.js'
 
 
 const store = createStore(rootReducer, {}, compose(
-    applyMiddleware(thunk),
-    composeWithDevTools()
+    applyMiddleware(thunk)
 ))
 
 export default store;
